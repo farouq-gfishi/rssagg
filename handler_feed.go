3b516c7f2dc400b4b2faf79536abfcedf2bd7081
farouq-gfishi <farouqgfishi@gmail.com>
2025-11-01 17:48:44 +0300

    add new endpoint to get all feeds

--- handler_feed.go
+++ handler_feed.go
@@ -34,3 +34,11 @@ func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Reques
 	}
 	responseWithJSON(w, 201, databaseFeedToFeed(feed))
 }
+
+func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request) {
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+	}
+	responseWithJSON(w, 200, databaseFeedsToFeeds(feeds))
+}
